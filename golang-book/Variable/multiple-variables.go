094453a7f837945ec3e372ab225c3606ee2135a2
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-03-07 17:30:16 +0900

    [UPDATE🚀] Variable: 여러개의 변수

---
+++ golang-book/Variable/multiple-variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a = 5
+		b = 10
+		c = 15
+	)
+
+	const (
+		x = 2
+		y = 4
+		z = 6
+	)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println("=======")
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+}
