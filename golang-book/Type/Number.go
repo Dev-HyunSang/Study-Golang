3bff6daed90d76477fcc069ef00ad9d6d0743f2c
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-03-07 13:00:03 +0900

    [UPDATE🚀] 타입: 숫자 연산자

---
+++ golang-book/Type/Number.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 덧셈
+	fmt.Println("1+1=", 1+1)
+	// 뺄셈
+	fmt.Println("1-1=", 2-1)
+	// 곱셈
+	fmt.Println("2*1=", 2*1)
+	// 나눗셈
+	fmt.Println("4/2=", 4/2)
+	// 나머지
+	fmt.Println("5%2", 5%2)
+}
