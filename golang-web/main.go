9e7a20aa520b8463a1889ac25c13778b4b10e5df
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-03-14 18:40:52 +0900

    [UPDATE🚀] TEST 환경 구축

--- golang-web/main.go
+++ golang-web/main.go
@@ -1,62 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
+
+	"github.com/tuckersGo/goWeb/web3/myapp"
 )
 
-type User struct {
-	FirstName string    `json:"first_name"` // String => JSON Annotation
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-/*
-Not Annotation
-{"FirstName":"","LastName":"","Email":"hyun.sang@parkhyunsang.com","CreatedAt":"2021-03-13T16:23:07.319954+09:00"}
-String => JSON Annotation
-{"first_name":"HyunSang","last_name":"Park","email":"hyun.sang@parkhyunsang.com","created_at":"2021-03-13T16:30:24.447374+09:00"}
-*/
-
-type fooHandler struct{}
-
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	user := new(User)
-	err := json.NewDecoder(r.Body).Decode(user)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Bad Request: ", err)
-		return
-	}
-	user.CreatedAt = time.Now()
-	data, _ := json.Marshal(user)
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, string(data))
-}
-
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "World"
-	}
-	// 127.0.0.1/bar?name=Hello
-	// => Hello Hello
-	fmt.Fprint(w, "Hello ", name, "!")
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
-
-	mux.HandleFunc("/bar", barHandler)
-
-	mux.Handle("/foo", &fooHandler{})
-
-	http.ListenAndServe(":3000", mux)
+	http.ListenAndServe(":3000", myapp.NewHttpHandler())
 }
